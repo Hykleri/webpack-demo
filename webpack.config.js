d7c54dd2b72856da058fc8e1beed5a25db72205d
Hykleri <1102223889@qq.com>
2019-02-16 15:07:27 +0800

    初始化

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// webpack配置文件
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// 需要暴露一个对象，注意，这里我用的暴露的方式还是 commonjs 方式
+module.exports = {
+  // 模式
+  mode: 'production',   // development | production
+
+  // 入口
+  entry: './src/index.js',
+
+  // 出口
+  output: {
+    // 出口位置（绝对路劲）
+    path: path.resolve(__dirname, './dist'), //项目根目录下的dist文件
+    filename: 'bundle.js', //打包之后文件的名字
+  },
+
+  // 插件
+  plugins: [
+    // 自动生成一个 html 文件在 出口的位置，并且会自动在这个生成的文件中引入 打包生成的js文件。
+    new HtmlWebpackPlugin({
+      title: '小可爱',
+      filename: 'abc.html',
+      template: './index.html' // 将index.html作为生成的abc.html的模板
+    })
+  ]
+}
